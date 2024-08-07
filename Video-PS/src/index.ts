d090aa6711e423a7b3d9c2c6a0dca44fb7c1fa4f
Advay Singh <advay@umich.edu>
2024-08-06 19:03:38 -0500

    res files with express and fluent

--- Video-PS/src/index.ts
+++ Video-PS/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import Ffmpeg from "fluent-ffmpeg";
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const url = "http://localhost";
 
 app.get("/", (req, res) => {
-  res.send("Hello Client!");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
